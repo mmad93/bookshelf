47c9517b0f47e99ce0b29191b0aeba76f623c100
mmad93 <m.taba1508@gmail.com>
2022-02-23 10:09:59 -0800

    this is added by defining a name for the url of my git

--- src/Components/ErrorNotFound/ErrorNotFound.js
+++ src/Components/ErrorNotFound/ErrorNotFound.js
@@ -3,6 +3,7 @@ const ErrorNotFound = () => {
     <div>
         <h1>This Page Does not Exist, would you like to go home?</h1>
         <h2>have you seen the git hub repository?</h2>
+        <h3>the name is also added for the repository</h3>
     </div>
   )
 }
