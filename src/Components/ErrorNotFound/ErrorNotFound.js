8a49389b383670af5228dbd686243f63e0968f59
mmad93 <m.taba1508@gmail.com>
2022-02-23 10:00:25 -0800

    a message is added to error page file

--- src/Components/ErrorNotFound/ErrorNotFound.js
+++ src/Components/ErrorNotFound/ErrorNotFound.js
@@ -2,6 +2,7 @@ const ErrorNotFound = () => {
   return (
     <div>
         <h1>This Page Does not Exist, would you like to go home?</h1>
+        <h2>have you seen the git hub repository?</h2>
     </div>
   )
 }
